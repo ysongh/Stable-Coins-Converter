eb211a0d7b33cb8bfe36c63f7c47320bb2281327
You Song Hou <yousonghou@gmail.com>
2021-11-04 00:21:52 -0400

    update hardhat.config to deploy contracts on Rinkeby Testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +18,25 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 4
+    },
+    // npx hardhat run scripts/sample-script.js --network rinkeby
+    rinkeby: {
+      url: `https://eth-rinkeby.gateway.pokt.network/v1/lb/${process.env.POKT_NETWORK_KEY}`,
+      accounts: [process.env.PRIVATEKEY],
+      gasPrice: 8000000000     // Default is 'auto' which breaks chains without the london hardfork
+    }
+  },
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
