4e276f4f882f008481d4435935aa1323324dc913
You Song Hou <yousonghou@gmail.com>
2021-11-03 22:39:36 -0400

    install 'ethers' and 'web3modal' for connecting to wallet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal'
 
 function App() {
+  const [address, setAddress] = useState('');
+
+  const connectWallet = async () => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);  
+    console.log(provider);
+
+    const { chainId } = await provider.getNetwork();
+    console.log(chainId);
+
+    const signer = provider.getSigner();
+    const walletAddress = await signer.getAddress();
+    setAddress(walletAddress);
+  }
+
   return (
     <div>
       <h1>Stable Coins Converter</h1>
+        <button
+          type="primary"
+          onClick={connectWallet}
+        >
+          {address ? address : "Connect to Wallet"}
+        </button>
     </div>
   );
 }
